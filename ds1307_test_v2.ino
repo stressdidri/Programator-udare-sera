b3afce34671d718891e9ad1e83f0d662bc0a7de7
Adrian <tudorache_adrian24@yahoo.com>
2019-05-02 21:53:27 +0300

    Update ds1307_test_v2.ino
    Util ar fi sa printeze errorile si pe LCD in caz de ai un contact imperfect prin fire

--- ds1307_test_v2.ino
+++ ds1307_test_v2.ino
@@ -23,11 +23,13 @@ void setup () {
    digitalWrite(Relay, HIGH); // Iesirea pentru releu este definita pentru modul de releu de 5V (functionare cu logica inversa)
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
+    lcd.print("Couldn't find RTC!");
     while (1);
   }
 
   if (! rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
+    lcd.print("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
     // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
